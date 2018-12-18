e571c256963d99842ad264d05059986a61eb9c43
Patrick Kranz <patrick-kranz@live.de>
2018-12-18 01:27:03 +0100

    Fix StackOverflowEception

--- src/DiceTool/Tables/TransformTable.cs
+++ src/DiceTool/Tables/TransformTable.cs
@@ -5,14 +5,14 @@ namespace Dice.Tables
 {
     internal class TransformTable<TFrom, TTo> : Table
     {
-        private readonly TransformState<TFrom, TTo> original;
+        private readonly TransformState<TFrom, TTo> state;
         private readonly P<TTo> pTo;
         private readonly Func<TFrom, TTo> func;
         private readonly P<TFrom> pFrom;
 
         public TransformTable(TransformState<TFrom, TTo> original, P<TFrom> PFrom, P<TTo> newP, Func<TFrom, TTo> func)
         {
-            this.original = original;
+            this.state = original;
             this.pFrom = PFrom;
             this.pTo = newP;
             this.func = func;
@@ -20,21 +20,21 @@ namespace Dice.Tables
 
         public override int GetCount(in WhileManager manager)
         {
-            return this.original.GetTable(this.pFrom, manager).GetCount();
+            return this.state.Parent.GetTable(this.pFrom, manager).GetCount();
         }
 
         public override object GetValue(IP p, int index, in WhileManager manager)
         {
             if (p.Id == this.pTo.Id)
             {
-                var originalValue = this.original.GetTable(this.pFrom, manager).GetValue(this.pFrom, index);
+                var originalValue = this.state.Parent.GetTable(this.pFrom, manager).GetValue(this.pFrom, index);
                 return this.func(originalValue)!;
             }
 
-            return this.original.GetTable(this.pFrom, manager).GetValue(p, index);
+            return this.state.Parent.GetTable(this.pFrom, manager).GetValue(p, index);
         }
 
-        protected override bool InternalContains(IP key, in WhileManager manager) => key.Id == this.pTo.Id ? true : this.original.GetTable(this.pFrom, manager).Contains(key);
+        protected override bool InternalContains(IP key, in WhileManager manager) => key.Id == this.pTo.Id ? true : this.state.Parent.GetTable(this.pFrom, manager).Contains(key);
     }
 
 }
